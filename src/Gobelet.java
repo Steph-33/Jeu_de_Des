55c1881483ed7ccb0642429f4f2bd52796beda2c
Stéphane PATERNA <paterna@club-internet.fr>
2022-07-11 11:10:18 +0200

    First commit classe_gobelet

---
+++ src/Gobelet.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+public class Gobelet {
+    // La classe Gobelet représente le gobelet utilisé dans la partie.
+    //Celui-ci a :
+    //Deux attributs :
+    //- Valeur : nombre représentant la valeur d'un lancer du gobelet
+    //- Des : tableau de dés qui contient un certain nombre de dés
+    //Un constructeur
+    //- Avec un argument
+    //o constructor(nb_des) : initialise la valeur du gobelet à 0, génère le nombre de
+    //dés nécessaires à la partie et les ajoute au tableau des
+    //Ainsi que 3 méthodes :
+    //- get_valeur() : renvoie la valeur du gobelet
+    //- lancer() : change la valeur des dés du gobelet ; met à jour la valeur du gobelet
+    //- afficher_score() : affiche en console le score du dernier lancé de gobelet
+
+    // Attributs
+    private int valeur;
+    private List<De> des;
+
+    // Constructeurs
+    public Gobelet(){
+
+    }
+    public Gobelet(List<De> nb_des){
+        this.valeur = 0;
+        this.des = nb_des;
+    }
+
+    // Getters et Setters
+
+
+    // Méthodes
+
+    /**
+     * La méthode get_valeur() renvoie la valeur du gobelet
+     * @return valeur
+     */
+    public double get_valeur(){
+        return this.valeur;
+    }
+    //- lancer() : change la valeur des dés du gobelet ; met à jour la valeur du gobelet
+    public double lancer(){
+
+        De.lancer();
+
+        return this.valeur;
+    }
+
+    //- afficher_score() : affiche en console le score du dernier lancé de gobelet
+
+    public void afficher_score(){
+        System.out.println("Le dernier score est : " + this.lancer());
+    }
+
+}
